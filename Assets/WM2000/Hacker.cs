4f0e52aebb1289c185d6f5181d48a3df2c814aef
Jessica M <contact@jmvazquez.com>
2017-11-09 23:02:42 -0500

    Refactored code by simplifying functions and improved player experience with new screens and standard messages. Also added some debug logs.

--- Assets/WM2000/Hacker.cs
+++ Assets/WM2000/Hacker.cs
@@ -4,11 +4,50 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour {
 	// Enums
-	enum Screen { MainMenu, Password, Win };
+	enum Screen { MainMenu, Password, Win, GameOver };
+
+	// Structs
+	struct Level {
+		public string title;
+		public string[] passwords;
+		public int maxTries;
+		public string slogan;
+
+		public Level(string t, string[]pwds, int mt, string s) {
+			title = t;
+			passwords = pwds;
+			maxTries = mt;
+			slogan = s;
+		}
+	}
+
+	// Game configuration data
+	const string MENU_HINT = "\nYou may type \"menu\" at any time.";
+	Level LEVEL1 = new Level(
+		"Depto. de Hacienda de Puerto Rico", 
+		new string[] { "admin", "password", "hacienda", "test", "taxes" }, 
+		5,
+		"Integridad - Lealtad - Justicia"
+	);
+	Level LEVEL2 = new Level(
+		"Evertec, Inc. [Intranet]", 
+		new string[] { "software", "transaction", "athmovil", "popular", "windows" }, 
+		7,
+		"Transaction Solutions Simplified"
+	);
+	Level LEVEL3 = new Level(
+		"NASA", 
+		new string[] { "environment", "telescope", "astronauts", "starfield", "exploration" }, 
+		3,
+		"For the Benefit of All"
+	);
 
 	// Game state
 	Screen currentScreen = Screen.MainMenu;
 	int currentLevel;
+	string currentPassword;
+	int numTries = 0;
+	int maxTries = 5;
 
 	// Use this for initialization
 	void Start () {
@@ -16,7 +55,7 @@ public class Hacker : MonoBehaviour {
 	}
 
 	void OnUserInput(string input) {
-		print(input);
+		Debug.Log(input);
 
 		input = input.ToLower ().Trim ();
 
@@ -28,7 +67,7 @@ public class Hacker : MonoBehaviour {
 		}
 
 		else if (currentScreen == Screen.Password) {
-			// soon
+			CheckPassword (input);
 		}
 	}
 
@@ -42,57 +81,116 @@ public class Hacker : MonoBehaviour {
 		Terminal.WriteLine (greeting + "\n");
 
 		Terminal.WriteLine ("Please select a place to start \nyour journey into hacking:\n");
-		Terminal.WriteLine ("1) Depto. de Hacienda de Puerto Rico \n2) Evertec, Inc. \n3) NASA\n");
+		Terminal.WriteLine ("1) " + LEVEL1.title + " \n2) " + LEVEL2.title + " \n3) " + LEVEL3.title + "\n");
 		Terminal.WriteLine ("Enter your selection's number below:");
 	}
 
 	void HandleMainMenu (string input)
 	{
-		switch (input) {
-		case "1":
-			StartLevel (1);
-			break;
-		case "2":
-			StartLevel (2);
-			break;
-		case "3":
-			StartLevel (3);
-			break;
-		case "menu":
-			ShowMainMenu ();
-			break;
-		case "valar morghulis":
-			Terminal.WriteLine ("Valar Dohaeris.");
-			break;
-		default:
-			Terminal.WriteLine ("Please choose a valid option:");
-			print ("Invalid level number.");
-			break;
+		bool isValidLevel = (input == "1" || input == "2" || input == "3");
+		if (isValidLevel) {
+			currentLevel = int.Parse (input);
+			StartLevel ();				
+		} else {
+			switch (input) {
+				case "valar morghulis":
+					Terminal.WriteLine ("Valar Dohaeris.");
+					break;
+				default:
+					Terminal.WriteLine ("Please choose a valid option:");
+					Debug.LogError("Invalid level number.");
+					break;
+			}
 		}
 	}
 
-	void StartLevel(int level) {
+	void StartLevel() {
 		// Update game state
 		currentScreen = Screen.Password;
-		currentLevel = level;
 
 		// Level info
 		string title = "";
 
-		switch (level) {
+		// Level setup
+		SetupLevel (ref title);
+
+		// Show screen
+		Terminal.ClearScreen ();
+		Terminal.WriteLine (title);
+		Terminal.WriteLine (MENU_HINT);
+		Terminal.WriteLine ("\nPlease enter password. \nHint: " + currentPassword.Anagram());
+	}
+
+	void CheckPassword(string input) {
+		numTries++;
+		if (input == currentPassword) {
+			WinGame ();
+		} else {
+			if (numTries == maxTries) {
+				GameOver ();
+			} else {
+				Terminal.WriteLine ("\nWrong password. " + (maxTries - numTries).ToString() + " tries left. \nHint: " + currentPassword.Anagram());
+			}
+		}
+	}
+
+	void SetupLevel (ref string title)
+	{
+		numTries = 0;
+		switch (currentLevel) {
+		case 1:
+			title = LEVEL1.title.ToUpper ();
+			currentPassword = LEVEL1.passwords [Random.Range (0, LEVEL1.passwords.Length)];
+			maxTries = LEVEL1.maxTries;
+			break;
+		case 2:
+			title = LEVEL2.title.ToUpper ();
+			currentPassword = LEVEL2.passwords [Random.Range (0, LEVEL2.passwords.Length)];
+			maxTries = LEVEL2.maxTries;
+			break;
+		case 3:
+			title = LEVEL3.title.ToUpper ();
+			currentPassword = LEVEL3.passwords [Random.Range (0, LEVEL3.passwords.Length)];
+			maxTries = LEVEL3.maxTries;
+			break;
+		default:
+			Debug.LogError ("Invalid level number in password screen.");
+			break;
+		}
+	}
+
+	void WinGame ()
+	{
+		currentScreen = Screen.Win;
+		Terminal.ClearScreen ();
+		Terminal.WriteLine ("CORRECT!\n");
+		ShowLevelReward ();
+		Terminal.WriteLine(MENU_HINT);
+	}
+
+	void ShowLevelReward()
+	{
+		switch (currentLevel) {
 			case 1:
-				title = "DEPARTAMENTO DE HACIENDA\n DE PUERTO RICO";
+				Terminal.WriteLine ("\"" + LEVEL1.slogan + "\"");
 				break;
 			case 2:
-				title = "EVERTEC, INC. [INTRANET]";
+				Terminal.WriteLine ("\"" + LEVEL2.slogan + "\"");
 				break;
 			case 3:
-				title = "NASA";
+				Terminal.WriteLine ("\"" + LEVEL3.slogan + "\"");
+				break;
+			default:
+				Debug.LogError ("Invalid level reached.");
 				break;
 		}
+	}
 
-		Terminal.ClearScreen ();
-		Terminal.WriteLine (title + "\n");
-		Terminal.WriteLine ("Please enter password:");
+	void GameOver()
+	{
+		currentScreen = Screen.GameOver;
+		Terminal.ClearScreen();
+		Terminal.WriteLine ("GAME OVER.");
+		Terminal.WriteLine (MENU_HINT);
 	}
 }
